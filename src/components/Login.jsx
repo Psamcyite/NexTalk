95d08bb4959d2fb8da2bb3bbda2a30cfd1b1e6cc
Psamcyite <amojoayokunle@gmail.com>
2023-12-31 12:24:54 -0500

    initial commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
-import firebase from "firebase/app";
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import 'firebase/app';
 import "firebase/auth";
 
 import { auth } from '../firebase';
